c7dcc40f7ace46c05c6f364197d16d59a6692781
Luisin <luisoliveira@unipam.edu.br>
2022-02-25 09:40:55 -0300

    projeto base - herbario

---
+++ js/navbar.js
@@ -0,0 +1,40 @@
+class ResponsiveMenu{
+  constructor(responsiveMenu, navList, navListLi){
+    this.responsiveMenu = document.querySelector(responsiveMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navListLi);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks(){
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = 'naviLinkFade 0.5s ease forwards 0.3s');
+    });
+  }
+
+  handleClick(){
+  this.navList.classList.toggle(this.activeClass);
+  this.animateLinks();
+}
+
+  addClickEvent(){
+    this.responsiveMenu.addEventListener("click", this.handleClick);
+  }
+    init(){
+      if(this.responsiveMenu){
+        this.addClickEvent();
+      }
+      return this;
+    }
+}
+
+const responsiveMenu = new ResponsiveMenu(
+  ".responsive-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+responsiveMenu.init();
